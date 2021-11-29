c719c8e4191185ae2bca1fde9b4c1927fdc4b260
tanghanzheng <tanghanzheng@landsky.com>
2021-11-29 14:14:14 +0800

    sample

---
+++ sample/src/main/java/com/github/linyuzai/concept/sample/ConceptSampleApplication.java
@@ -0,0 +1,12 @@
+package com.github.linyuzai.concept.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConceptSampleApplication.class, args);
+    }
+}
