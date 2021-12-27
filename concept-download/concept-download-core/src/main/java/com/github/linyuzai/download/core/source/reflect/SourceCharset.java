01df1dca9dbdef084d4c5fd77cfce595f4317f84
tanghanzheng <tanghanzheng@landsky.com>
2021-12-27 11:06:42 +0800

    Reflection

--- concept-download/concept-download-core/src/main/java/com/github/linyuzai/download/core/source/reflect/SourceCharset.java
+++ concept-download/concept-download-core/src/main/java/com/github/linyuzai/download/core/source/reflect/SourceCharset.java
@@ -1,18 +1,12 @@
 package com.github.linyuzai.download.core.source.reflect;
 
-import com.github.linyuzai.download.core.source.reflect.conversion.StringToCharsetValueConvertor;
-
 import java.lang.annotation.*;
 import java.nio.charset.Charset;
 
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@SourceReflection(
-        methodName = "setCharset",
-        methodParameterType = Charset.class,
-        fieldName = "charset",
-        convertor = StringToCharsetValueConvertor.class)
+@SourceReflection(methodName = "setCharset", methodParameterType = Charset.class, fieldName = "charset")
 public @interface SourceCharset {
 
 }
--- concept-download/concept-download-core/src/main/java/com/github/linyuzai/download/core/source/reflect/conversion/StringToCharsetValueConvertor.java
+++ concept-download/concept-download-core/src/main/java/com/github/linyuzai/download/core/source/reflect/conversion/StringToCharsetValueConvertor.java
@@ -4,6 +4,16 @@ import java.nio.charset.Charset;
 
 public class StringToCharsetValueConvertor implements ValueConvertor<String, Charset> {
 
+    private static final StringToCharsetValueConvertor instance = new StringToCharsetValueConvertor();
+
+    public static StringToCharsetValueConvertor getInstance() {
+        return instance;
+    }
+
+    static {
+        ValueConversion.helper().register(getInstance());
+    }
+
     @Override
     public Charset convert(String value) {
         return Charset.forName(value);
