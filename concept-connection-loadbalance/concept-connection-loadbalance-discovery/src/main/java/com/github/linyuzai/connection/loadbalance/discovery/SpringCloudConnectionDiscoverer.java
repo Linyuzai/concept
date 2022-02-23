dc01f5f824c9f41f0cd0c18eaf0efc77f00155d7
tanghanzheng <tanghanzheng@landsky.com>
2022-02-23 17:41:44 +0800

    spring cloud 3.1.1 spring boot 2.6.3

--- concept-connection-loadbalance/concept-connection-loadbalance-discovery/src/main/java/com/github/linyuzai/connection/loadbalance/discovery/SpringCloudConnectionDiscoverer.java
+++ concept-connection-loadbalance/concept-connection-loadbalance-discovery/src/main/java/com/github/linyuzai/connection/loadbalance/discovery/SpringCloudConnectionDiscoverer.java
@@ -1,4 +1,9 @@
 package com.github.linyuzai.connection.loadbalance.discovery;
 
+import com.github.linyuzai.connection.loadbalance.core.concept.Connection;
+
 public class SpringCloudConnectionDiscoverer {
+
+    private Connection connection;
+
 }
