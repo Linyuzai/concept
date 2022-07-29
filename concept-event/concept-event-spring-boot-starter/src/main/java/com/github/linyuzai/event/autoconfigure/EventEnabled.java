400442ca551fe10fb620a4b51dc70b116fae5dfa
tanghanzheng <tanghanzheng@landsky.com>
2022-07-29 17:33:16 +0800

    event

--- concept-event/concept-event-spring-boot-starter/src/main/java/com/github/linyuzai/event/autoconfigure/EventEnabled.java
+++ concept-event/concept-event-spring-boot-starter/src/main/java/com/github/linyuzai/event/autoconfigure/EventEnabled.java
@@ -1,10 +1,11 @@
 package com.github.linyuzai.event.autoconfigure;
 
 import com.github.linyuzai.event.kafka.autoconfigure.KafkaEventAutoConfiguration;
+import com.github.linyuzai.event.rabbitmq.autoconfigure.RabbitEventAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@AutoConfigureBefore(KafkaEventAutoConfiguration.class)
+@AutoConfigureBefore({KafkaEventAutoConfiguration.class, RabbitEventAutoConfiguration.class})
 public class EventEnabled {
 }
