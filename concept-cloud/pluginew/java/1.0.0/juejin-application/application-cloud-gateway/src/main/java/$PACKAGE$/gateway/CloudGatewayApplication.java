e278f6c7b0b1c475357264248334433a2ab995f2
tanghanzheng <tanghanzheng@landsky.com>
2023-05-18 13:22:13 +0800

    concept cloud pluginew template application

---
+++ concept-cloud/pluginew/java/1.0.0/juejin-application/application-cloud-gateway/src/main/java/$PACKAGE$/gateway/CloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package $PACKAGE$.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 微服务网关启动类
+ */
+@SpringBootApplication
+public class CloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class, args);
+    }
+}
---
+++ concept-cloud/pluginew/java/1.0.0/juejin-application/application-cloud-user/src/main/java/$PACKAGE$/cloud/CloudUserApplication.java
@@ -0,0 +1,15 @@
+package $PACKAGE$.cloud;
+
+import $PACKAGE$.basic.cloud.$CLASS$CloudApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * 微服务用户和消息启动类
+ */
+@$CLASS$CloudApplication
+public class CloudUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudUserApplication.class, args);
+    }
+}
